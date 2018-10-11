fca94b83062de26476f695aa28eb0c9d9703b164
Kamal Advani <kamal@namingcrisis.net>
2018-10-12 09:45:54 +1100

    Add test for Assert util.

---
+++ j-child/src/test/java/net/namingcrisis/sweetchildomine/jchild/AssertTest.java
@@ -0,0 +1,32 @@
+package net.namingcrisis.sweetchildomine.jchild;
+
+import org.junit.Test;
+
+import static net.namingcrisis.sweetchildomine.jchild.TestUtil.mustFail;
+
+public final class AssertTest {
+
+  @Test
+  public void assertTrue() {
+    Assert.assertTrue(true, () -> {
+      throw new AssertionError("Never expected to be called!");
+    });
+
+    mustFail(
+      () -> Assert.assertTrue(false, () -> "Failed condition"),
+      "Failed condition"
+    );
+  }
+
+  @Test
+  public void assertFalse() {
+    Assert.assertFalse(false, () -> {
+      throw new AssertionError("Never expected to be called!");
+    });
+
+    mustFail(
+      () -> Assert.assertFalse(true, () -> "Failed condition"),
+      "Failed condition"
+    );
+  }
+}
