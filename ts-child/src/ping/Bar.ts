de3c71d33db1e5f5983ea9b45c2edd5c231fe765
Kamal Advani <kamal@namingcrisis.net>
2018-10-12 14:31:57 +1100

    Trash placeholder TS files and tests.

--- ts-child/src/ping/Bar.ts
+++
@@ -1,7 +0,0 @@
-import Foo from '@app/Foo';
-
-export class Bar {
-  constructor(public foo: Foo) {
-    this.foo = new Foo();
-  }
-}
--- ts-child/test/Foo.test.ts
+++
@@ -1,7 +0,0 @@
-import Foo from "../src/Foo";
-
-describe("Foo", () => {
-  it("should return 5", () => {
-    expect(new Foo().bar()).toBe(5);
-  });
-});
